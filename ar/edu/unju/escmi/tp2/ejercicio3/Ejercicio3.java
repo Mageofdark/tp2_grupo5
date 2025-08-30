c105ee8260f258d29c61b9c03859d9d16b8e450e
Yato <darkflamesmage23@gmail.com>
2025-08-30 05:31:57 -0300

    Se agregan los ejercicios 3 y 8 : Enrique Real

--- ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
+++ ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
@@ -1,7 +1,20 @@
 package ar.edu.unju.escmi.tp2.ejercicio3;
 
-public class Ejercicio3 {
-    public static void main(String[] args) {
 
+public class Ejercicio3 {
+    private final float PI = 3.14f;
+    private final short CODIGO_POSTAL = 4600;
+    private final byte DIAS_SEMANA = 7;
+    private final byte MESES_AÑO = 12;
+// No es nesesario usar static para definir constantes
+
+    public static void main(String[] args) {
+        Ejercicio3 ej = new Ejercicio3();
+        System.out.println("El valor de PI es: " + ej.PI);
+        System.out.println("El valor del codigo postal es: " + ej.CODIGO_POSTAL);
+        System.out.println("El valor de los dias de la semana es: " + ej.DIAS_SEMANA);
+        System.out.println("El valor de los meses del año es: " + ej.MESES_AÑO);
     }
+    
+    
 }
