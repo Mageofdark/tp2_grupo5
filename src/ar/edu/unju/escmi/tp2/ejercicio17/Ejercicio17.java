d3f6cf7f27021d5cc6237e011852e21fa93fbac0
matias12131 <velazquezmatiasclass@gmail.com>
2025-09-01 16:58:14 -0300

    Explicacion del ejercicio 17 por Matias Velazquez

--- src/ar/edu/unju/escmi/tp2/ejercicio17/Ejercicio17.java
+++ src/ar/edu/unju/escmi/tp2/ejercicio17/Ejercicio17.java
@@ -3,7 +3,7 @@ package ar.edu.unju.escmi.tp2.ejercicio17;
 public class Ejercicio17 {
     public static void main(String[] args) {
 
-        Integer num1 = 50;
+        Integer num1 = 20;
         Integer num2 = 20;
 
         System.out.println("Referencia de num1 es: "+ System.identityHashCode(num1));
@@ -22,3 +22,9 @@ public class Ejercicio17 {
         }
     }
 }
+
+/*
+ * Si dos o mas objetos Integer tienen el mismo valor dentro del rango [-128, 127 ] java lo almacena en cache para ahorrar memoria y mejorar el rendimiento.
+ * Por eso, en num1 == num2 sus referencias son iguales, es decir, apuntan al mismo objeto. pero si elegimos un numero afuera de ese rango
+ * las referencias ya no seran iguales, sin importar si num1 y num2 tienen el mismo valor.
+*/
